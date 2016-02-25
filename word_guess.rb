d70ff988f51d35408b1dd9dfc4934363b69f3bdb
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-02-25 14:10:08 -0800

    Create, initialize game, check if guess is word method

--- word_guess.rb
+++ word_guess.rb
@@ -1,2 +1,34 @@
-def guess
+@word = "subcontinental"
+@number_tries = 6
+class GuessWord
+  attr_accessor :guess
+
+  def initialize(word)
+    @word = word
+  end
+
+  def user_guess
+    puts "Please enter a letter or word guess"
+    @guess = gets.chomp.downcase
+
+  end
+
+  def check_guess_if_word
+    if @guess.length > 1
+      if guess == word #for sting, change if array
+        #CORRECT GUESS METHOD
+      else
+        #INCORRECT GUESS METHOD
+      end
+    else
+      check_letter
+    end
+  end
+
+  def check_letter
+    if @word.include?(guess)
+      #CORRECT GUESS METHOD
+    end
+  end
+
 end
